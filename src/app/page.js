d098a625d37fc7e8beeb1f1b59d36e8f7a3b71f4
Bobcoder0010 <dellizulter@gmail.com>
2024-12-25 13:42:46 +0545

    next 3rd day but the laptop ram is more usage

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
-import Button from '@/components/button'
-import '@/styles/styles.css'
+// import Button from '@/components/button'
+import Image from 'next/image'
+// import '@/styles/styles.css'
  
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <Button />
-    </div>
-  )
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <h1>Hello World</h1>
+//       <Button />
+//     </div>
+//   )
+// }
+export default function page (){
+  return <image src="./src/public/file.svg" alt="image"/>
+  
 }
